41627fb135394552c3da46beadbb40b5de756350
Aryan Chouhan <aryanchouhan955@gmail.com>
2026-04-13 06:09:21 +0000

    Changes

--- src/pages/StudyRoom.tsx
+++ src/pages/StudyRoom.tsx
@@ -41,6 +41,9 @@ export default function StudyRoom() {
           <Clock className="mr-1 h-4 w-4" /> Timer
         </Button>
       </div>
+      <div className="border-b border-border bg-card px-4 py-2">
+        <SharedTopicEditor roomId={roomId || ""} />
+      </div>
 
       <div className="border-b border-border bg-card p-3 lg:hidden">
         <div className="space-y-3">
